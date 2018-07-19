76d56364105d7bbb567bb908685f5a54f6ef7c9e
Karen West <atribecalledkwest.97@gmail.com>
2018-07-19 02:38:49 -0400

    Add setup.py
    Move this to setuptools

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+def get_requirements():
+	with open("requirements.txt", "r") as f:
+		return [requirement.strip() for requirement in f.read().split("\n") if requirement.strip() != ""]
+
+def get_version():
+	with open("VERSION", "r") as f:
+		return f.read().strip()
+
+setup(
+	name="pinkdeck-helper",
+	version=get_version(),
+	install_requires=get_requirements(),
+	python_requires=">3.4",
+	entry_points={
+		"console_scripts": [
+		]
+	}
+)
